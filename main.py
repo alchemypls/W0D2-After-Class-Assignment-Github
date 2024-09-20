eeeca2169396fc597236b217c0ad2d02f1f415d5
alchemypls <alchemy838@gmail.com>
2024-09-19 21:33:03 -0700

    ok this should cause a merge conflict

--- main.py
+++ main.py
@@ -1,2 +1,9 @@
 def hello_world():
-    return "Hello, from Richard!"
+    return "Hello from Richard!"
+
+def greet_person(name):
+    return f"Hello, {name}"
+
+if __name__ == "__main__":
+    print(hello_world())
+    print(greet_person(__name__))
--- test_main.py
+++ test_main.py
@@ -1,9 +1,13 @@
 import unittest
-from main import hello_world
+from main import greet_person, hello_world
 
 class TestHelloWorld(unittest.TestCase):
     def test_Hello_world(self):
-        self.assertEqual(hello_world(), "Hello from Richard!")
+        self.assertEqual(hello_world(), "Hello From Richard")
 
-if __name__ == '__mina__':
+    def test_Greet_person(self):
+        self.assertEqual(greet_person(self), f"Hello, {self}")
+        
+
+if __name__ == '__main__':
     unittest.main()
